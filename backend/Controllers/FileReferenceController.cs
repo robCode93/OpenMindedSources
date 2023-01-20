d0b973d8a468f138869749ead66e1eee10229c26
RobCoding <robertbrock93@yahoo.de>
2023-01-20 21:44:14 +0100

    #backend - FileReferenceService - Upload

--- backend/Controllers/FileReferenceController.cs
+++ backend/Controllers/FileReferenceController.cs
@@ -1,4 +1,6 @@
-﻿using backend.ServiceInterfaces;
+﻿using backend.CRUDModels;
+using backend.DetailsModels;
+using backend.ServiceInterfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +19,29 @@ namespace backend.Controllers
 
         // ########## GET-Methoden ##########
         // ########## CREATE-Methoden ##########
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(FileReferenceDetails))]
+        public IActionResult UploadFileToDatabase(IFormFile file, CreateFileReferenceModel createModel)
+        {
+            try
+            {
+                //using var ms = new MemoryStream();
+                var mimeType = file.ContentType;
+
+                //file.CopyTo(ms);
+
+                //ms.Seek(0, SeekOrigin.Begin);
+                //ms.Position = 0;
+
+                var model = _fileReferenceService.UploadFileToDatabase(file.OpenReadStream(), mimeType, createModel);
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
         // ########## DELETE-Methoden ##########
     }
 }
