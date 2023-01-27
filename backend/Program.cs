8a1aab21212f42748ec5ebfdcbc2c335c368a2a9
RobCoding <robertbrock93@yahoo.de>
2023-01-27 23:15:18 +0100

    #database-config

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,8 @@
+using backend.Models;
+using backend.ServiceInterfaces;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<OpenMindServerContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("OpenMindConnection")));
+
+builder.Services.AddScoped<IFileReferenceService, FileReferenceService>();
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddScoped<ISourceCategoryService, SourceCategoryService>();
+builder.Services.AddScoped<ISourceService, SourceService>();
+builder.Services.AddScoped<ISubCategoryService, SubCategoryService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +27,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().Build());
 }
 
 app.UseAuthorization();
